0d62a4d5c64a31e884721687ffd66663665a53e3
SergeyK <sw1ple@mail.ru>
2021-07-01 13:16:57 +0300

    feat: console.log docker variables

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import './registerServiceWorker';
 import router from './router';
 import Maska from 'maska'
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.API_URL);
+console.log(process.env.SOCKET_PORT);
+
 
 const app = createApp(App)
     .use(router)
